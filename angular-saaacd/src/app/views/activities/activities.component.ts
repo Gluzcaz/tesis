7602e718b3f0c126b9895b3257e41ccbd2cbf475
luz <luz.celezte@gmail.com>
2020-05-29 00:54:33 -0500

    Added the following features on the Table: Search, Sort, Pagination, Selection.

--- angular-saaacd/src/app/views/activities/activities.component.ts
+++ angular-saaacd/src/app/views/activities/activities.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { ConfirmDialogModel, ConfirmDialogComponent } from '../confirmDialog/confirm-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
@@ -8,6 +8,11 @@ import { ActivityService } from '../../services/Activity.service';
 
 import { Observable, of } from 'rxjs';
 
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatFormFieldControl} from '@angular/material/form-field';
+
 @Component({
   selector: 'app-activities',
   templateUrl: './activities.component.html',
@@ -16,21 +21,33 @@ import { Observable, of } from 'rxjs';
 export class ActivitiesComponent implements OnInit {
   activities: Activity[];
   selectedActivity : Activity;
-
-  constructor(private activityService: ActivityService) { }
+  
+  //Table Elements
+  displayedColumns: string[] = ['id', 'comentario'];
+  dataSource: MatTableDataSource<Activity>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  
+  constructor(private activityService: ActivityService) {   
+  }
   
   ngOnInit() {
-    this.getActivities();
+	this.getActivities();
   }
 
   getActivities(): void {
     this.activityService.getActivities()
-    .subscribe(activities => this.activities = activities);
+    .subscribe(activities =>{ 
+		this.activities = activities;
+		this.updateDataSource();
+	});
   }
   
   confirmDialog(): void {
 	this.activities = this.activities.filter(a => a !== this.selectedActivity); //PENDIENTE VALIDACIÓN: ELIMINAR DE LA LISTA SOLO CUANDO SE ELIMINO EN DB
-   	this.activityService.openDialog(this.selectedActivity);
+   	this.updateDataSource();
+	
+	this.activityService.openDialog(this.selectedActivity);
 	this.selectedActivity = undefined;
   }
   
@@ -38,4 +55,21 @@ export class ActivitiesComponent implements OnInit {
 	this.selectedActivity = a;
 	console.log("Table selection:", a.id);
   }
+  
+  updateDataSource(){
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(this.activities);
+	this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
 }
